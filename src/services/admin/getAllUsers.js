190457920219c855031692a6a00e35cffa91d73b
Fede <fredez1991@gmail.com>
2024-10-03 22:00:40 -0300

    admin and more features

--- src/services/admin/getAllUsers.js
+++ src/services/admin/getAllUsers.js
@@ -5,8 +5,9 @@ const { ERROR_WHILE_GETTING_USERS } = errorCodes.adminErrors
 
 export const getAllUsers = async(req, res) => {
     try {
-        const users = await userService.getAll()
-        return res.status(StatusCodes.OK).json({ users })
+        const employeesUsers = await userService.getAllEmployeeUsers()
+        const commonUsers = await userService.getAllCommonUsers()
+        return res.status(StatusCodes.OK).json({ employeesUsers, commonUsers })
     } catch (err) {
         const errors = [{ msg: ERROR_WHILE_GETTING_USERS }]
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ errors })
